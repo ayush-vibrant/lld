9b2ea0956bb868970ecfc1c0e3e1ac7bc47b48e6
ayush.sharma <ayush.sharm@olx.com>
2023-03-08 12:31:43 +0530

    add explanation in abstractFactory pattern

--- src/abstractFactory/GUIAbstractFactory.java
+++ src/abstractFactory/GUIAbstractFactory.java
@@ -5,6 +5,7 @@ import abstractFactory.simpleFactory.OsFactory;
 import abstractFactory.simpleFactory.WindowsOsFactory;
 
 public class GUIAbstractFactory {
+    // Abstract factory would return factory. Once you have factory, you can create concrete objects from that factory.
     static OsFactory osFactory;
 
     public static OsFactory createFactory(String os) {
