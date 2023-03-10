85588c97605626f4ac7ee3bcd6ef1a17b050e4f5
ayush.sharma <ayush.sharm@olx.com>
2023-03-10 14:49:11 +0530

    add singleton pattern

---
+++ src/singleton/MySingleton.java
@@ -0,0 +1,15 @@
+package singleton;
+
+public class MySingleton {
+    private static MySingleton instance = null;
+
+    private MySingleton() {}
+
+    public static MySingleton getInstance() {
+        if (instance == null) {
+            instance = new MySingleton();
+        }
+        return instance;
+    }
+}
+
---
+++ src/singleton/SingletonApplication.java
@@ -0,0 +1,9 @@
+package singleton;
+
+public class SingletonApplication {
+    public static void main(String[] args) {
+        MySingleton mySingleton = MySingleton.getInstance();
+        MySingleton mySingleton2 = MySingleton.getInstance();
+        System.out.println(mySingleton == mySingleton2);
+    }
+}
